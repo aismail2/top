68ccff4e5058ba81f16d2fd02c1cd2a9bfdb4ea6
Abdallah Ismail <abdallah.ismail@sesame.org.jo>
2016-08-29 14:21:27 +0300

    Moved all sequencers to this ioc.

--- iocBoot/ioc/st.cmd
+++ iocBoot/ioc/st.cmd
@@ -16,6 +16,7 @@ dbLoadRecords "db/mrf-vmeevr230-ttl.db"
 dbLoadRecords "db/mrf-vmeevr230-univ.db"
 dbLoadRecords "db/mrf-vmeevr230-cml.db"
 dbLoadRecords "db/power_seq.db"
+dbLoadRecords("db/power-seq-extkic.db")
 
 evgConfigure "EVG0" "gbl-ti-evg" "2000" "125"
 evrConfigure "EVR0" "gbl-ti-evr1" "2000" "125"
@@ -25,4 +26,17 @@ asSetFilename("/home/control/epics/support/access/common.acf")
 
 iocInit
 
+seq safe_polarity_switcher, "CELL=C01,DEVICE=CR1"
+seq safe_polarity_switcher, "CELL=C02,DEVICE=CR1"
+seq safe_polarity_switcher, "CELL=C03,DEVICE=CR1"
+seq safe_polarity_switcher, "CELL=C04,DEVICE=CR1"
+seq safe_polarity_switcher, "CELL=C05,DEVICE=CR1"
+seq safe_polarity_switcher, "CELL=C06,DEVICE=CR1"
+seq extkic
+seq microtron_polarity_switcher, "DEVICE=MI-PS-DC1,CHANNEL=1"
+seq microtron_polarity_switcher, "DEVICE=MI-PS-DC1,CHANNEL=2"
+seq microtron_polarity_switcher, "DEVICE=MI-PS-DC2,CHANNEL=1"
+seq microtron_polarity_switcher, "DEVICE=MI-PS-DC2,CHANNEL=2"
+seq waterflowMonitor
 seq power_seq
+seq freq_shift
